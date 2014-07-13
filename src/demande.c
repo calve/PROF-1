8a094295d7af0ed3188559771104a01c3fd8e7b6
WebTogz <antonin.carette@gmail.com>
2014-07-13 19:44:14 +0200

    Séparation des fonctions de demande (Login+Mdp) dans un module à part entière

---
+++ src/demande.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "suppr.h"
+
+/** 
+  * \def LONGUEUR_ID
+  * \brief Macro concernant la longueur maximale d'un identifiant étudiant - ici: 20 caractères
+  */
+#define LONGUEUR_ID 20
+
+/**
+ * \def LONGUEUR_MDP
+ * \brief Macro concernant la longueur maximale d'un mot-de-passe étudiant - ici: 15 caractères
+ */
+#define LONGUEUR_MDP 15
+
+/**
+ * \fn void demandeLogin(char* idFIL)
+ * \brief Fonction permettant de demander le login de l'utilisateur
+ *
+ * \param idFIL Une chaîne de caractères (20 caractères), destinée à recevoir le login de l'utilisateur
+ */
+void demandeLogin(char* idFIL) {
+	
+	printf("Entrez votre identifiant FIL:");
+	fgets(idFIL, LONGUEUR_ID, stdin);
+	supprimeCaractere(idFIL, '\n');
+
+}
+
+/**
+ * \fn void demandeMDP(char* mdpFIL)
+ * \brief Fonction permettant de demander le mot-de-passe associé au login de l'utilisateur
+ *
+ * \param mdpFIL Une chaîne de caractères (15 caractères), destinée à recevoir le mot-de-passe de l'utilisateur
+ */
+void demandeMDP(char* mdpFIL) {
+
+	printf("Entrez votre mot de passe FIL:");
+	fgets(mdpFIL, LONGUEUR_MDP, stdin);
+	supprimeCaractere(mdpFIL, '\n');
+
+}
---
+++ src/demande.h
@@ -0,0 +1,10 @@
+#ifndef DEMANDE
+#define DEMANDE
+
+#define LONGUEUR_ID 20
+#define LONGUEUR_MDP 15
+
+void demandeLogin(char*);
+void demandeMDP(char*);
+
+#endif
