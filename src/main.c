d97960da6849b11b16da3dc85f8f6e1782fe619d
WebTogz <antonin.carette@gmail.com>
2014-07-14 18:41:24 +0200

    Suppression du code concernant la structure String, et ses fonctions

--- src/main.c
+++ src/main.c
@@ -19,6 +19,7 @@
 
 #include "demande.h"
 #include "suppr.h"
+#include "structStr.h"
 
 /**
  * \def FALSE
@@ -48,37 +49,8 @@
 #define DEBUG 0
 #endif
 
-struct string {
-  char *ptr;
-  size_t len;
-};
-
 struct zip;
-
-void initStructString(struct string *s) {
-  s->len = 0;
-  s->ptr = malloc(s->len+1);
-  if (s->ptr == NULL) {
-    printf("ERREUR: malloc!\n");
-    exit(EXIT_FAILURE);
-  }
-  s->ptr[0] = '\0';
-}
-
-static size_t writeFonction(void *ptr, size_t size, size_t nmemb, struct string *s)
-{
-  size_t new_len = s->len + size*nmemb;
-  s->ptr = realloc(s->ptr, new_len+1);
-  if (s->ptr == NULL) {
-    printf("ERREUR: realloc!\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->ptr+s->len, ptr, size*nmemb);
-  s->ptr[new_len] = '\0';
-  s->len = new_len;
-
-  return size*nmemb;
-}
+struct string;
 
 /**
  * \fn static long tailleFichier(char* cheminFichier)
@@ -432,6 +404,13 @@ int main() {
 	
 	/*
 	Récupération de la page HTML dans un fichier - Parse + récupération de la liste des matières - affichage
+	*/
+	/*char** optionsPROF = parseFichierHTML(str);*/
+
+	/*
+	int choix = fgets("Votre choix?");
+
+	char** RendusOption = parseOption(str, optionsPROF[choix]);
 	*/
 
 	/*
